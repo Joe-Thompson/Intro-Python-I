e3ee97ab0b72c4f8fbc434ea4486fc445077b8a0
Joe-Thompson <bmo3696@gmail.com>
2020-08-03 21:04:11 -0400

    hello world

--- src/00_hello.py
+++ src/00_hello.py
@@ -1,1 +1,11 @@
 # Print "Hello, world!" to your terminal
+
+print('Hello, world!')
+greeting = 'Hello, world, from inside a function'
+
+
+def my_greeting(greet):
+    print(greet)
+
+
+my_greeting(greeting)
